c7c0b65bc107b0f22d0cf638fac7b519338dbc25
emmabeanween <32688713+emmabeanween@users.noreply.github.com>
2019-02-26 20:52:42 -0800

    Update top_college_web_scraping.R

--- top_college_web_scraping.R
+++ top_college_web_scraping.R
@@ -3,17 +3,23 @@ library(rvest)
 library(stringr)
 library(tidyr)
 library(gsubfn)
+#get the page
 colleges_page <- read_html("https://www.usnews.com/best-colleges/rankings/national-universities")
+#the college names
 titles <- colleges_page %>% html_nodes("h3") %>% html_text() %>% str_trim()
 final_titles <- titles[1:10] 
+#the rankings of the colleges
 rankings <- colleges_page %>% html_nodes(xpath = '//*[@class="display-block block-flush text-strong"]') %>% html_text() %>% str_trim()
 rankings <- rankings[1:10]
 rankings <- parse_number(rankings)
 table_rank <- table(rankings)
+#get schools that tie
 ties <- rankings[table_rank > 1]
+#the polished rankings - add 'tie' to ranking if a tie
 final_rankings <- sapply(rankings, function(rank){ if(rank %in% ties){return(paste(rank, "(tie)"))}else{return(rank)}   })
 locations <- colleges_page %>% html_nodes(xpath = '//*[@class="display-block block-normal text-small"]') %>% html_text() 
 final_locations <- locations %>% str_trim()
+#get the tuitions of the schools
 tuition <- colleges_page %>% html_nodes(xpath='//*[@class="display-inline-for-medium-up inline-right-tight-for-medium-up border-right-for-medium-up"]') %>% html_text() %>% str_trim()
 tuition <- tuition[1:20]
 tuitions <- str_extract(tuition[1:20], '^\\$[0-9,]*')
@@ -24,6 +30,7 @@ undergrad_enrollment <- as.numeric(gsub("[^\\d]+", "", undergrad_enrollment, per
 first_bit <- sapply(undergrad_enrollment, function(string){ if(nchar(string) > 4){paste0(substring(string, 1, 2), ",")}else{paste0(substring(string, 1, 1), ",")}   })
 second_bit <- sapply(undergrad_enrollment, function(string){if(nchar(string)>4){substring(string, 3)}else{substring(string, 2)}})
 final_undergrad_enrollment <- paste0(first_bit, second_bit)
+#the descriptions of the colleges
 descriptions <- colleges_page %>% html_nodes(xpath='//*[@class="block-normal text-small show-for-medium-up ellipsis"]') %>% html_text() %>% str_trim()
 descriptions <- str_remove(descriptions, "[\\n]")
 descriptions <- str_remove(descriptions, "more")
